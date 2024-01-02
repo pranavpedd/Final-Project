58f2f518486d20903c18a20e7179de80f16d3535
drnxv <87783549+drnxv@users.noreply.github.com>
2024-01-02 00:28:49 -0500

    formatted js

--- spotifyRecs/server.js
+++ spotifyRecs/server.js
@@ -1,13 +1,13 @@
 // required libraries
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
-const favicon = require("serve-favicon");
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+const favicon = require('serve-favicon');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_DB_CONNECTION_STRING;
 const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 const dbCollection = {
@@ -18,10 +18,10 @@ const dbCollection = {
 // spotify stuff
 const clientID = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-const redirectURI = "https://spotrecs.onrender.com/callback";
+const redirectURI = 'https://spotrecs.onrender.com/callback';
 
 // default encoding
-process.stdin.setEncoding("utf-8");
+process.stdin.setEncoding('utf-8');
 
 // invalid number of commands
 if (process.argv.length !== 3) {
@@ -32,17 +32,17 @@ if (process.argv.length !== 3) {
 // port number and message to show server is starting
 const port = process.argv[2];
 console.log(`Web server started and running at http://localhost:${port}/`);
-const prompt = "Stop to shutdown the server: ";
+const prompt = 'Stop to shutdown the server: ';
 
 // show prompt and make it listen for 'stop' command
 process.stdout.write(prompt);
-process.stdin.on("readable", async function () {
+process.stdin.on('readable', async function () {
   let input = process.stdin.read();
   if (input !== null) {
     let cmd = input.trim();
-    if (cmd === "stop") {
-      process.stdout.write("Shutting down the server\n");
-      process.stdout.write("Closing connection to database\n");
+    if (cmd === 'stop') {
+      process.stdout.write('Shutting down the server\n');
+      process.stdout.write('Closing connection to database\n');
       await client.close();
       process.exit(0);
     } else {
@@ -59,24 +59,23 @@ async function main() {
   try {
     await client.connect();
   } catch (error) {
-    console.log("Failed to connect.", error);
+    console.log('Failed to connect.', error);
   }
 }
 
 main();
 
 // setting templates directory
-app.set("views", path.resolve(__dirname, "templates"));
-app.set("view engine", "ejs");
+app.set('views', path.resolve(__dirname, 'templates'));
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(favicon(path.join(__dirname, "images", "spotify logo real.ico")));
+app.use(favicon(path.join(__dirname, 'images', 'spotify logo real.ico')));
 
 // helper function
 const randomString = (length) => {
-  let result = "";
+  let result = '';
 
-  const chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < length; i++) {
     result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
@@ -85,15 +84,15 @@ const randomString = (length) => {
 };
 
 // get and post requests
-app.get("/", (request, response) => {
+app.get('/', (request, response) => {
   const variables = {
     port: port,
   };
 
-  response.render("index", variables);
+  response.render('index', variables);
 });
 
-app.post("/submit", async (request, response) => {
+app.post('/submit', async (request, response) => {
   let user = {
     name: request.body.name,
     email: request.body.email,
@@ -102,7 +101,7 @@ app.post("/submit", async (request, response) => {
 
   try {
     await insertUser(client, dbCollection, user);
-    response.redirect("/login");
+    response.redirect('/login');
   } catch (error) {
     console.error(error);
   }
@@ -113,15 +112,12 @@ async function insertUser(client, dbCollection, user) {
 }
 
 async function getTracks(token) {
-  const response = await fetch(
-    "https://api.spotify.com/v1/me/top/tracks?limit=5",
-    {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    }
-  );
-  
+  const response = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=5', {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  });
+
   let topTracks = [];
   const data = await response.json();
   const tracks = data.items;
@@ -144,31 +140,27 @@ async function getTracks(token) {
 
   async function getRecommendations() {
     return (
-      await fetchWebApi(
-        `v1/recommendations?limit=5&seed_tracks=${topTracks.join(",")}`,
-        "GET"
-      )
+      await fetchWebApi(`v1/recommendations?limit=5&seed_tracks=${topTracks.join(',')}`, 'GET')
     ).tracks;
   }
 
   const recommendedTracks = await getRecommendations();
   let ret = recommendedTracks.map(
-    ({ name, artists }) =>
-      `${name} by ${artists.map((artist) => artist.name).join(", ")}`
-  )
+    ({ name, artists }) => `${name} by ${artists.map((artist) => artist.name).join(', ')}`
+  );
 
   return ret;
 }
 
 // TODO: spotify api integration
-app.get("/login", (request, response) => {
+app.get('/login', (request, response) => {
   const state = randomString(16);
-  const scope = "user-read-private user-read-email user-top-read";
+  const scope = 'user-read-private user-read-email user-top-read';
   response.redirect(
-    "https://accounts.spotify.com/authorize?" +
+    'https://accounts.spotify.com/authorize?' +
       new URLSearchParams({
         client_id: clientID,
-        response_type: "code",
+        response_type: 'code',
         redirect_uri: redirectURI,
         state: state,
         scope: scope,
@@ -176,27 +168,27 @@ app.get("/login", (request, response) => {
   );
 });
 
-app.get("/callback", (req, res) => {
+app.get('/callback', (req, res) => {
   const code = req.query.code || null;
   const state = req.query.state || null;
 
   if (state === null) {
     res.redirect(
-      "/#" +
+      '/#' +
         new URLSearchParams({
-          error: "state_mismatch",
+          error: 'state_mismatch',
         }).toString()
     );
   } else {
     const authOptions = {
-      url: "https://accounts.spotify.com/api/token",
+      url: 'https://accounts.spotify.com/api/token',
       form: {
         code: code,
         redirect_uri: redirectURI,
-        grant_type: "authorization_code",
+        grant_type: 'authorization_code',
       },
       headers: {
-        Authorization: "Basic " + Buffer.from(clientID + ":" + clientSecret).toString("base64"),
+        Authorization: 'Basic ' + Buffer.from(clientID + ':' + clientSecret).toString('base64'),
       },
       json: true,
     };
@@ -208,12 +200,12 @@ app.get("/callback", (req, res) => {
         let tracks = await getTracks(accessToken);
         let artists = [];
         let songs = [];
-        
-        tracks.map(item => {
+
+        tracks.map((item) => {
           let parts = item.split(' by ');
           artists.push(parts[1]);
           songs.push(parts[0]);
-        })
+        });
 
         const variables = {
           song1: songs[0],
@@ -228,7 +220,7 @@ app.get("/callback", (req, res) => {
           artist5: artists[4],
         };
 
-        res.render("taste", variables);
+        res.render('taste', variables);
       } else {
         res.send(`Error accessing token: ${error}`);
       }
@@ -236,16 +228,16 @@ app.get("/callback", (req, res) => {
   }
 });
 
-app.get("/refresh_token", function (req, res) {
+app.get('/refresh_token', function (req, res) {
   let refresh_token = req.query.refresh_token;
   let authOptions = {
-    url: "https://accounts.spotify.com/api/token",
+    url: 'https://accounts.spotify.com/api/token',
     headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      Authorization: "Basic " + new Buffer.from(clientID + ":" + clientSecret).toString("base64"),
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: 'Basic ' + new Buffer.from(clientID + ':' + clientSecret).toString('base64'),
     },
     form: {
-      grant_type: "refresh_token",
+      grant_type: 'refresh_token',
       refresh_token: refresh_token,
     },
     json: true,
